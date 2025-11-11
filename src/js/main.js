1902e48a742c0ab95b0524840dfb84dd8fbf1afe
Monica Coca <mcocapelaz@gmail.com>
2025-11-11 18:08:01 +0100

    display products function js added

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,47 @@
-'use strict';
+"use strict";
 
 // SECCIÓN DE QUERY-SELECTOR
 // Éstos son los elementos que nos traemos de la página HTML y usamos en el código
 
-const searchInput = document.querySelector('.js-search-input');
-const searchButton = document.querySelector('.js-search-btn');
-const addButton = document.querySelector('.js-add-product');
-
+const searchInput = document.querySelector(".js-search-input");
+const searchButton = document.querySelector(".js-search-btn");
+const addButton = document.querySelector(".js-add-product");
+const resultList = document.querySelector(".js-results-list");
 
 // SECCIÓN DE DATOS
 // Variables globales que almacenan la información principal de la aplicación
 // y se usan por todo el fichero.
 
-let products= [];
-let shoppingCartProducts= [];
-
-
+let products = [];
+let shoppingCartProducts = [];
 
 // SECCIÓN DE FUNCIONES
+
+function displayProducts(listToShow) {
+  resultList.innerHTML += "";
+  for (let i = 0; i < listToShow.length; i++) {
+    const item = listToShow[i];
+    resultList.innerHTML = `<li>
+<div class="js-product-card" data-id="${item.id}"></div>
+<img src= "${item.image}" alt="${item.title}">
+<h3>"${item.title}"</h3>>
+<span class="js-title-card">${item.title}</span>
+<span class="js-price-card">${item.price}</span>
+</li>`;
+  }
+}
+
 // Éstas son funciones:
 //   - con código auxiliar
 //   - con código que usaremos en los eventos
 //   - para pintar (render) en la página.
 
-
-
 // SECCIÓN DE FUNCIONES DE EVENTOS
 // Aquí van las funciones handler/manejadoras de eventos
-
-
 
 // SECCIÓN DE EVENTOS
 // Éstos son los eventos a los que reacciona la página
 // Los más comunes son: click (en botones, enlaces), input (en ídem) y submit (en form)
-
-
 
 // SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
 // Este código se ejecutará cuando se carga la página
@@ -42,12 +49,11 @@ let shoppingCartProducts= [];
 //   - Pedir datos al servidor
 //   - Pintar (render) elementos en la página
 
-fetch('https://fakestoreapi.com/products')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+fetch("https://fakestoreapi.com/products")
+  .then((response) => response.json())
+  .then((data) => {
+    products = data;
+    displayProducts(products);
+  })
 
-
-
-
-
+  .catch((error) => console.error("Error:", error));
